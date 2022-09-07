21f8c6ac056e58dabbdae0de2a75578b99403de5
Aditi <aditigarg305@gmail.com>
2022-09-08 00:58:10 +0530

    Adding changes for sketch picker

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { SketchPicker } from 'react-color';
+import { useState } from "react";
+
+function App() {
+    const [color, setColor] = useState();
+    const handleChangeComplete = (color) => {
+        setColor(color.hex);
+    }
+  return (
+    <div  style={{
+        backgroundColor: color,
+        height: '1000px',
+        width:  '1000px',
+    }}>
+      <SketchPicker
+          color = {color}
+          onChangeComplete={ handleChangeComplete }
+      />
+    </div>
+  );
+}
+
+export default App;
